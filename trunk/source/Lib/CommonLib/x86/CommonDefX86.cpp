09c1c218393f44ddfd0ecb0f534b77e3d373793e
hhi <hhi@3b01ee18-6722-49cb-b174-11e6e4ae8ea4>
2017-10-23 05:12:19 +0000

    fixed headers to be ITU compatible

--- trunk/source/Lib/CommonLib/x86/CommonDefX86.cpp
+++ trunk/source/Lib/CommonLib/x86/CommonDefX86.cpp
@@ -1,24 +1,36 @@
-/**
-  \ingroup CommonLib
-  \file    CommondefX86.cpp
-  \brief   This file contains the SIMD x86 common used functions.
-  \author
-  \date
-
-  Copyright:
-  2012 Fraunhofer Institute for Telecommunications, Heinrich-Hertz-Institut (HHI)
-  The copyright of this software source code is the property of HHI.
-  This software may be used and/or copied only with the written permission
-  of HHI and in accordance with the terms and conditions stipulated
-  in the agreement/contract under which the software has been supplied.
-  The software distributed under this license is distributed on an "AS IS" basis,
-  WITHOUT WARRANTY OF ANY KIND, either expressed or implied.
+/* The copyright in this software is being made available under the BSD
+* License, included below. This software may be subject to other third party
+* and contributor rights, including patent rights, and no such rights are
+* granted under this license.
+*
+* Copyright (c) 2010-2017, ITU/ISO/IEC
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*  * Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*  * Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*  * Neither the name of the ITU/ISO/IEC nor the names of its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+* THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
-// #include <stdio.h>
-//#include <atomic>
-//#include <mutex>
 #include <sstream>
 #include <map>
 #include <iostream>
